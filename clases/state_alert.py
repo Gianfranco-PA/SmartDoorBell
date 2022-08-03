894a60413ebff9539e20c7d119922e1accfd4ced
gianfranco <gianfranco.perez.aquise@gmail.com>
2022-08-03 01:17:30 -0500

    debounce test

--- clases/state_alert.py
+++ clases/state_alert.py
@@ -1,22 +1,24 @@
 
 
 from clases.sound import Sound
+from test_bot.debounce import Debounce
 
 
 class State_Alert:
     
     def __init__(self, sound:Sound):
-        self.num_pushButton=0
+        self.num_pushButton=[0]
         self.alarm_activate=False
         self.alarm=sound
+        self.debounce=Debounce(self.num_pushButton,3,self.restart)
         
-    #Debounce
         
     def to_update(self):
-        self.num_pushButton+=1
+        self.debounce.run()
+        self.num_pushButton[0]+=1
         if self.alarm_activate:
             self.alarm.stop()
-            self.num_pushButton=0
+            self.restart()
             self.alarm_activate=False
         if not self.alarm_activate and self.num_pushButton>=3:
             self.alarm.loop()
@@ -24,3 +26,6 @@ class State_Alert:
             
     def getIsState(self):
         return self.alarm_activate
+    
+    def restart(self):
+        self.num_pushButton=[0]
